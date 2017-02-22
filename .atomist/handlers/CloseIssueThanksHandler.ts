5dbb5e5b8712103253bcb0bc67d7dbe1d888c592
Russ Miles <russ@russmiles.com>
2017-02-22 06:17:39 -0500

    Completed custom close issue handler and also reintroduced the overridden default handlers

--- .atomist/handlers/CloseIssueThanksHandler.ts
+++ .atomist/handlers/CloseIssueThanksHandler.ts
@@ -2,18 +2,15 @@ import {Atomist} from '@atomist/rug/operations/Handler'
 import {TreeNode} from '@atomist/rug/tree/PathExpression'
 declare var atomist: Atomist
 
-atomist.on<TreeNode, TreeNode>("/Issue()[/resolvedBy::Commit()/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]?[/by::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?][/belongsTo::Repo()/channel::ChatChannel()]", m => {
+atomist.on<TreeNode, TreeNode>("/Issue()/belongsTo::Repo()/channel::ChatChannel()", m => {
    let issue = m.root() as any
 
    if (issue.state() != "closed") {
      return
    }
 
-   let message = atomist.messageBuilder().regarding(issue)
-   
-   message.say("Thanks " + issue.resolvedBy() + " for closing this issue!")
-
-   let cid = "issue/" + issue.belongsTo().owner() + "/" + issue.belongsTo().name() + "/" + issue.number()
-
-   message.withCorrelationId(cid).send()
+   atomist.messageBuilder()
+    .say("Thanks for closing this issue on " +
+         issue.belongsTo().name())
+    .on(issue.belongsTo().channel().id()).send()
 })
